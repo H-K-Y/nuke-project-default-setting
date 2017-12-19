879cd474107c4e835fdac17b0728685c1620d247
HaoKangYuan <33424676+wandering-wind@users.noreply.github.com>
2017-12-20 00:06:47 +0800

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,20 @@
+
+
+
+######################################################################
+#                                                                    #
+#   https://github.com/wandering-wind/nuke-project-default-setting   #
+#                                                                    #
+######################################################################
+
+
+import Set_project
+Set_project.Set_project()
+
+
+
+
+
+
+
+
